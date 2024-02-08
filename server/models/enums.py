db689faab28b45cd603ce4fb8269cc7c244ff407
Kimberly Palmer <kimberlylpalmer@gmail.com>
2024-02-07 19:14:48 -0800

    trying to figure out why meal_type is not working

---
+++ server/models/enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class MealTypeEnum(Enum):
+    BEEF = "Beef"
+    POULTRY = "Poultry"
+    SEAFOOD = "Seafood"
+    PORK = "Pork"
+    LAMB = "Lamb"
+    APPETIZERS = "Appetizers"
+    SIDES = "Side Dishes"
+    SOUPS = "Soups"
+    PASTA_GRAINS = "Pasta, Grains, & Rice"
+    POTATOES = "Potatoes, Beans, & Legumes"
+    SALADS_VEGETABLES = "Salads & Vegetables"
+    BREADS = "Breads"
+    SWEETS = "Desserts"
+    DRINKS = "Drinks"
