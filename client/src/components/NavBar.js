39d6d5f03e8e7cd1d37858f19b04c81d71eed631
Kimberly Palmer <kimberlylpalmer@gmail.com>
2024-02-07 16:09:40 -0800

    adding flask-bcrypt to requirements.txt

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useAuth } from './AuthContext';
 import "../styles.css";
 import LogoutButton from './LogoutButton';
